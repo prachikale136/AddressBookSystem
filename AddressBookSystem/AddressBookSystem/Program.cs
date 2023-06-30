927373fe89233e776d35cae4091642702134f0cf
Prachi <prachikale136@gmail.com>
2023-06-30 11:58:27 +0530

    [Prachi] Add: Add conatct using the console

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -11,10 +11,27 @@ namespace AddressBookSystem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the address Book System Problem");
+
+            Console.WriteLine("Enter the First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter the City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter the State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Phone Number");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Email");
+            string email = Console.ReadLine();
+
             AddressBookMain addressBookMain = new AddressBookMain();
-            addressBookMain.AddContact("prachi", "kale", "bhf", "Nagpur", "MH", 441804, 01234456789, "abc@gmail.com");
-            addressBookMain.PrintList();
-            Console.ReadLine();
+
+            addressBookMain.AddContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
         }
     }
 }
