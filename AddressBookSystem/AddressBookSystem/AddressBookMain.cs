277717254549dc3d407d7ca368260bc9001c6a21
Prachi <prachikale136@gmail.com>
2023-06-30 11:52:07 +0530

    [Prachi] Add: Create contact of address book

---
+++ AddressBookSystem/AddressBookSystem/AddressBookMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookMain
+    {
+        public List<Contact> contactList = new List<Contact>();  //here create a list for contact
+        public void AddContact(string firstName, string lastName, string address, String city, string state, int zip, long phoneNumber, string email)
+        {
+            Contact contact = new Contact();  // create object of contact class
+
+            // calling variables by using object
+            contact.firstName = firstName;
+            contact.lastName = lastName;
+            contact.address = address;
+            contact.city = city;
+            contact.state = state;
+            contact.zip = zip;
+            contact.phoneNumber = phoneNumber;
+            contact.email = email;
+            contactList.Add(contact);
+
+        }
+
+        //method to print Conatct List
+
+        public void PrintList()
+        {
+            foreach (Contact contact in contactList)
+            {
+
+                Console.WriteLine("First Name :" + contact.firstName);
+                Console.WriteLine("LastNAme:" + contact.lastName);
+                Console.WriteLine("Address:" + contact.address);
+                Console.WriteLine("City:" + contact.city);
+                Console.WriteLine("State:" + contact.state);
+                Console.WriteLine("zip:" + contact.zip);
+                Console.WriteLine("PhoneNumber:" + contact.phoneNumber);
+                Console.WriteLine("Email" + contact.email);
+
+            }
+
+        }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address Book System Problem");
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.AddContact("prachi", "kale", "bhf", "Nagpur", "MH", 441804, 01234456789, "abc@gmail.com");
+            addressBookMain.PrintList();
+            Console.ReadLine();
+        }
+    }
+}
